d1ddfe350ede5226ace03f25f4fccc675f0dffc2
JasonPlayzs20 <103009819+JasonPlayzs20@users.noreply.github.com>
2024-07-26 12:32:53 -0400

    Start

---
+++ src/main/java/net/jason/learningprocess/LearningProcessDataGenerator.java
@@ -0,0 +1,11 @@
+package net.jason.learningprocess;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class LearningProcessDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
